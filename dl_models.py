0b42fd67bf3a2179a63fc3dd8d694db8dc8bb62f
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2022-09-08 15:09:05 -0300

    Update model to use new input shape. Update model to load words list from json file instead of text file. Add function to construct reponse object in server file. Add new words.json to contain words list.

--- dl_models.py
+++ dl_models.py
@@ -1,46 +1,53 @@
-import os
+import pandas as pd
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import LSTM, Dense
+from tensorflow.python.keras.optimizer_v2.adam import Adam
 
 
 class DLModel:
     def __init__(self) -> None:
-        self.words = self.get_dataset_words()
+        self.words = self.load_words_info_file('words.json')
         self.label_map = self.create_labels(self.words)
         self.sequence_length = 30
-        print(self.words)
         self.model = self.create_lstm_model()
 
     def create_lstm_model(self):
         model = Sequential()
-        model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(self.sequence_length, 1662)))
+        model.add(LSTM(64, return_sequences=True, activation='relu',
+                  input_shape=(self.sequence_length, 258)))
         model.add(LSTM(128, return_sequences=True, activation='relu'))
         model.add(LSTM(64, return_sequences=False, activation='relu'))
         model.add(Dense(64, activation='relu'))
         model.add(Dense(32, activation='relu'))
         model.add(Dense(len(self.words), activation='softmax'))
 
-        model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+        model.compile(optimizer=Adam(
+            learning_rate=0.0001
+        ), loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
         return model
 
     def create_labels(self, words):
         label_map = {label: num for num, label in enumerate(words)}
         return label_map
-    
+
     def load_weights(self, file_path):
         self.model.load_weights(file_path)
-    
+
     def predict(self, data):
         return self.model.predict(data)
-    
+
     def get_words_list(self):
         return self.words
-    
+
+    def load_words_info_file(self, file_path):
+        data = pd.read_json(file_path).set_index('word').to_dict()
+        return data['count'].keys()
+
     def get_dataset_words(self):
         words = []
-        with open('word_list.txt') as file:
+        with open('word_list.txt', encoding='utf8') as file:
             for line in file.readlines():
                 words.extend(line.split(','))
         return words
--- server.py
+++ server.py
@@ -1,21 +1,30 @@
-import os
-from socket import socket
-from cv2 import exp
-
 from flask import Flask
 from flask_socketio import SocketIO
 
 import numpy as np
 
-from server.dl_models import DLModel
+from dl_models import DLModel
 
 app = Flask(__name__)
 socketio = SocketIO(app=app, cors_allowed_origins='*')
 
+
+def construct_response_object(words, probabilities):
+    response = []
+    print(words)
+    print(probabilities)
+    for (i, word) in enumerate(words):
+        response.append(
+            {'word': word, 'probability': float(probabilities[0][i])})
+    print(response)
+    return response
+
+
 @socketio.on('connect')
 def test_connect():
     print("[!] Connection established with client.")
-    socketio.emit('after connect', {"connected":True})
+    socketio.emit('after connect', {"connected": True})
+
 
 @socketio.on('mediapipe-data')
 def receive_mediapipe_data(data):
@@ -23,11 +32,11 @@ def receive_mediapipe_data(data):
     new_data = np.expand_dims(np.asarray(data), axis=0)
     print(new_data.shape)
     res = model.predict(new_data)
-    word = model.get_words_list()[np.argmax(res)] 
-    print(word)
-    socketio.emit('prediction', word)
+    response = construct_response_object(model.get_words_list(), res)
+    socketio.emit('predictions', response)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     print('** Loading Model')
     model = DLModel()
     print('** Loading Model')
